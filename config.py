1b2adbb5658911cbb8d3a6098429bdac45131fd8
优秀 <bysen32@gmail.com>
2022-05-01 18:04:47 +0800

    update

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ def get_args():
     parser.add_argument("--use_fpn", default=False, type=bool)
     parser.add_argument("--use_ori", default=True, type=bool)
     parser.add_argument("--use_gcn", default=False, type=bool)
-    parser.add_argument("--use_contrast", default=True, type=bool)
+    parser.add_argument("--use_contrast", default=False, type=bool)
 
     parser.add_argument("--use_layers", 
         default=[False, False, False, False], type=list)
